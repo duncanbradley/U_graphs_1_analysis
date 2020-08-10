891b228592a74721119a70b8c995e7cc6dec58d1
Duncan Bradley <47359202+duncanbradley@users.noreply.github.com>
2020-08-10 17:38:07 +0100

    summary for demographic info

--- analysis_script.R
+++ analysis_script.R
@@ -8,11 +8,14 @@ library(ggeffects)
 library(parameters)
 library(effectsize)
 library(performance)
+library(janitor)
 
 # DATA WRANGLING ####
 
 # specifying conflict preference
 select <- dplyr::select
+rename <- dplyr::rename
+
 
 # read in csv
 df <- read_csv("U_graphs_1_data.csv",
@@ -72,7 +75,7 @@ df <- df %>%
   mutate(total_RT = pmax(slider_1.rt, slider_2.rt, slider_3.rt))
 
 # renaming columns
-df <- df %>% rename(
+df <- df %>% dplyr::rename(
     Gender = `Gender *`,
     Age = `Age *`,
     Education = starts_with("Highest level of education"))
@@ -105,9 +108,9 @@ stats_summary <- stats_summary %>%
                         "gp1_label" = "slider_1",
                         "gp2_label" = "slider_2",
                         "gp3_label" = "slider_3")) %>%
-  pivot_wider(names_from = label, values_from = c(mean, sd),
+  pivot_wider(names_from = label, values_from = c(mean, sd, min_value, max_value),
               names_glue = "{label}.{.value}") %>%
-  rename(graph_image = graph_id)
+  dplyr::rename(graph_image = graph_id)
 
 # renaming columns
 axis_summary <- axis_summary %>% rename(lower_lim = `1`,
@@ -237,7 +240,22 @@ df <- df %>%
 
 df <- df %>%
   rowwise() %>%
-  mutate(height = ((mean - lower_lim)/(upper_lim - lower_lim)))
+  mutate(midpoint = min_value + ((max_value - min_value)/2)) %>%
+  mutate(st_midpoint = ((midpoint - lower_lim)/(upper_lim - lower_lim)))
+
+df <- df %>%
+  rowwise() %>%
+  mutate(height = ((mean - lower_lim)/(upper_lim - lower_lim))) 
+
+df <- df %>%
+  rowwise() %>%
+  mutate(diff.est_mean = response - height,
+         diff.est_st_midpoint = response - st_midpoint)
+  
+mean(df$diff.est_mean)
+mean(df$diff.est_st_midpoint)
+
+
 
 df %>% ggplot(aes(x = z_score,
                   y = (estimate-mean),
@@ -250,9 +268,45 @@ df %>% ggplot(aes(x = difference,
                   colour = y_label)) +
   geom_jitter(alpha= 0.1) +
   facet_wrap(~ y_label, scales = "free_y")
+
+library(ggridges)
+df %>%
+  ggplot(aes(x = height,
+             y = is_ooo)) + 
+  geom_density_ridges(alpha = 0.5)
   
 
 # VISUALISATION AND ANALYSIS####
+
+# checking for missing values
+vis_miss(df)
+
+# coding 'is_ooo' and 'difference_sign' as factors
+df$is_ooo <- as.factor(df$is_ooo)
+df$difference_sign <- as.factor(df$difference_sign)
+
+# checking the distribution of the DV (z_score)
+descdist(df$z_score)
+# there is almost no skew but very high kurtosis - check effects on LMMs
+hist(df$z_score)
+
+df %>% ggplot(aes(x = z_score)) + 
+  geom_dotplot(binwidth = 0.5) +
+  scale_y_continuous(NULL, breaks = NULL)
+
+# summary stats
+mean(df$z_score)
+# on average, there is slight over-estimation
+sd(df$z_score)
+
+qts <- quantile(df$z_score,probs=c(.025,.975))
+hist(df$z_score)
+abline(v=qts[1],col="red")
+abline(v=qts[2],col="red")
+abline(v=mean(df$z_score),col="blue")
+qts
+CI(df$z_score, ci=0.95)
+
 
 # visualising z_scores individually for each participant
 # participant 71 produced the unusually high estimates
@@ -396,10 +450,10 @@ summary(model6)
 df %>%
   ggplot(aes(x = height,
              y = z_score)) +
-  #geom_jitter(alpha = 0.1) +
-  geom_smooth(method = lm ) +
+  geom_point(alpha = 0.05) +
+  geom_smooth(method = lm )
   #geom_smooth() +
-  stat_summary(fun = mean, size = 0.1)
+  #stat_summary(fun = mean, size = 0.1)
 
 model7 <- lmer(z_score ~ height +
                  (1 | participant) + 
@@ -419,8 +473,10 @@ df %>%
   ggplot(aes(x = height,
              y = z_score,
              colour = is_ooo)) +
-  geom_smooth(method = lm) +
-  facet_wrap(~ difference_sign)
+  geom_point(alpha = 0.01) +
+  geom_smooth(method = lm) 
+  #coord_cartesian(ylim = c(-2.5, 2.5)) 
+  #facet_wrap(~ difference_sign)
 
 model8 <- lmer(z_score ~ height*is_ooo +
                  (1 | participant) + 
@@ -440,6 +496,7 @@ df %>%
              y = z_score,
              colour = difference_sign)) +
   geom_smooth(method = lm) +
+  geom_point() +
   stat_summary(fun = mean, size = 0.1) +
   facet_wrap(~ is_ooo)
 
@@ -490,133 +547,26 @@ plot(compare_performance(model9a,
                          rank = TRUE))
 anova(model9d, model9_null)
 summary(model9d)
-# ANALYSIS ####
 
-# checking for missing values
-vis_miss(df)
+# investigating effect of learning:
+df %>%
+  ggplot(aes(x = presentation,
+             y = z_score)) +
+  stat_summary(fun = mean, size = 0.1, 
+               mapping = aes(colour = slider)) +
+  geom_smooth()
 
-# coding 'is_ooo' and 'difference_sign' as factors
-df$is_ooo <- as.factor(df$is_ooo)
-df$difference_sign <- as.factor(df$difference_sign)
-
-# checking the distribution of the DV (z_score)
-descdist(df$z_score)
-# there is almost no skew but very high kurtosis - check effects on LMMs
-hist(df$z_score)
-
-# summary stats
-mean(df$z_score)
-# on average, there is slight over-estimation
-sd(df$z_score)
-lCI <- mean(df$z_score) - ((sd(df$z_score))*1.96)
-uCI <- mean(df$z_score) + ((sd(df$z_score))*1.96)
-lCI
-uCI
-uCI - lCI
-# 95% of responses are within 3 cluster widths of the cluster centroid
-
-# Is there evidence for over-estimation changing as a function of y_ axis position?
-model1 <- lmer(z_score ~ height +
-                 (1 | participant) + 
-                 (1 | graph_image), 
-               data = df)
-model1_null <- lmer(z_score ~ 
-                      (1 | participant) + 
-                      (1 | graph_image), 
-                    data = df)
-anova(model1, model1_null)
-summary(model1)
-# yes
-
-
-
-model1 <- lmer(z_score ~ difference_sign +
-                 (1 | participant) + 
-                 (1 | graph_image), 
-               data = df)
-model1_null <- lmer(z_score ~ 
-                      (1 | participant) + 
-                      (1 | graph_image), 
-                    data = df)
-anova(model1, model1_null)
-summary(model1)
-
-# Does accuracy differ as a function of difference magnitude and sign?
-model2 <- lmer(z_score ~ difference*is_ooo +
+# no effect of learning
+model8 <- lmer(z_score ~ diff.est_mean + diff.est_st_midpoint +
                  (1 | participant) +
                  (1 | graph_image), 
                data = df)
-model2_null <- lmer(z_score ~ 
-                  (1 | participant) +
-                  (1 | graph_image), 
-                data = df)
-anova(model2, model2_null)
-summary(model2)
-# evidence supporting full model
-
-# is there an interaction between difference_sign and is_ooo?
-model3 <- lmer(z_score ~ difference_sign*is_ooo +
-                 (1 | participant) +
-                 (1 | graph_image),
-               data = df)
-model3_null <- lmer(z_score ~ 
-                 (1 | participant) +
-                 (1 | graph_image),
-               data = df)
-anova(model3_null, model3)
-summary(model3)# no evidence for full model
-
-# is there an interaction between height and is_ooo?
-model4 <- lmer(z_score ~ height*is_ooo +
-                 (1 | participant) +
-                 (1 | graph_image),
-               data = df)
-model4_null <- lmer(z_score ~ 
+model8_null <- lmer(z_score ~ 
                       (1 | participant) +
                       (1 | graph_image),
                     data = df)
-anova(model4_null, model4)
-summary(model4)
-# yes
-
-# is there an interaction between height and difference sign?
-model5 <- lmer(z_score ~ height*difference_sign +
-                 (1 | participant) +
-                 (1 | graph_image),
-               data = df)
-model5_null <- lmer(z_score ~ 
-                      (1 | participant) +
-                      (1 | graph_image),
-                    data = df)
-anova(model5_null, model5)
-summary(model5)
-# evidence supporting full model
-
-# including an interaction and two additive fixed effects
-model6 <- lmer(z_score ~ height*is_ooo + difference_sign + difference +
-                 (1 | participant) +
-                 (1 | graph_image),
-               data = df)
-model6_null <- lmer(z_score ~
-                      (1 | participant) +
-                      (1 | graph_image),
-                    data = df)
-anova(model6_null, model6)
-summary(model6)
-
-# removing the difference fixed effect 
-# in order to make way for random participant slopes
-model7 <- lmer(z_score ~ height*is_ooo + difference_sign +
-                 (1 + difference_sign | participant) +
-                 (1 | graph_image),
-               data = df)
-model7_null <- lmer(z_score ~ 
-                      (1 + difference_sign | participant) +
-                      (1 | graph_image),
-                    data = df)
-anova(model7_null, model7)
-summary(model7)
-anova(model7)
+anova(model8_null, model8)
+summary(model8)
 
 check_model(model7)
 model_performance(model7)
@@ -644,27 +594,110 @@ F_to_eta2(
   df_error = c(4733)
 )
 
-# investigating effect of learning:
+theme_mres <- function() {
+  theme_minimal(base_size=2.7, base_family="Helvetica") +
+    theme(plot.title = element_text(hjust = 0.5, size = 25),
+          axis.text.x = element_text(size = rel(3.5)),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(size = rel(1),
+                                      margin = unit(c(0, 1, 0, 0), "mm")),
+          axis.text.y = element_text(size = rel(3)),
+          axis.title = element_text(size = rel(3)),
+          panel.grid.major.x = element_blank(), # removes vertical gridlines
+          panel.grid.major.y = element_blank(), # removes major horizontal gridlines
+          panel.grid.minor.y = element_blank(), # removes minor horizontal gridlines
+          axis.text.y.right = element_text(colour = "white"),
+          axis.title.y.right = element_text(colour = "white",
+                                            margin = unit(c(0, 1, 0, 0), "mm")),
+          panel.border = element_rect(colour = "black", fill = NA),
+          aspect.ratio = 0.60
+    )
+}
+
+index <- 53
+
+# the following function takes the parameters of a dataset and uses them to 
+# build a dataframe. 
+create_df <- function(my_df){
+  set.seed(1234 + index)
+  y <- c(rnorm(24, my_df$gp1_pop_mean, my_df$gp1_pop_sd), 
+         rnorm(24, my_df$gp2_pop_mean, my_df$gp2_pop_sd),
+         rnorm(24, my_df$gp3_pop_mean, my_df$gp3_pop_sd))
+  x <- c(rep(my_df$gp1_label, 24), rep(my_df$gp2_label, 24),
+         rep(my_df$gp3_label, 24))
+  df <- as_tibble(cbind(y, x)) %>%
+    mutate(y = as.double(y)) %>%
+    mutate(x = as.factor(x))
+}
+
+my_graphs <- graphs_book1
+
+build_this_one <- my_graphs %>%
+  filter(graph_id == index) %>%
+  create_df() 
+
+ylab <- my_graphs[my_graphs$graph_id == index,]$y_label
+y_min <- my_graphs[my_graphs$graph_id == index,]$y_min
+y_max <- my_graphs[my_graphs$graph_id == index,]$y_max
+
+avg_est <- mean(df$z_score)
+
+build_this_one <- build_this_one %>%
+  group_by(x) %>%
+  mutate(est = mean(y) + (sd(y)*avg_est),
+         mean = mean(y),
+         val = (est - mean)/sd(y))
+
+set.seed(1234 +index)
+build_this_one %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_jitter(width = .1, alpha = .75, size = .25, height = 0) +
+  stat_summary(fun = mean, size = 0.05, colour = "red") +
+  stat_summary(aes(x = x, y = est),
+               fun = mean, size = 0.05, colour = "blue") +
+  labs(y = ylab) +
+  theme_mres() +
+  theme(plot.title = element_blank()) +
+  scale_y_continuous(sec.axis = dup_axis(),
+                     expand = c(0, 0), limits = c(y_min, y_max)) 
+
+
+# demographics
+unique(df$Gender)
 df %>%
-  ggplot(aes(x = presentation,
-             y = z_score)) +
-  stat_summary(fun = mean, size = 0.1, 
-               mapping = aes(colour = slider)) +
-  geom_smooth()
+  filter(Gender == "19") %>%
+  select(Age, participant) %>%
+  count(participant)
 
-# no effect of learning
-model8 <- lmer(z_score ~ presentation + 
-                 (1 | participant) +
-                 (1 | graph_image), 
-               data = df)
-model8_null <- lmer(z_score ~ 
-                      (1 | participant) +
-                      (1 | graph_image),
-                    data = df)
-anova(model8_null, model8)
+r <- df %>%
+  mutate(Gender = select(ifelse(starts_with("m", ignore.case = TRUE)), 3))
+         
 
+df <- df %>% mutate(Gender = tolower(Gender))
+unique(df$Gender)
+df <- df %>%
+  mutate(
+    Gender = recode(Gender, "m" = "male"),
+    Gender = recode(Gender, "19" = "NA"))  
 
+gender <- df %>%
+  group_by(Gender) %>%
+  summarise(n = n()) %>%
+  mutate(n = n/72)
+gender
+sum(gender$n)
 
-# Overlapping/non-overlapping clusters?
-# Bayes?
+rrr <- df %>% group_by(participant, Gender) %>% count()
+rrr
+unique(rrr$n)
+rrr %>%
+  filter(n == 81)
 
+p71 <- df %>%
+  filter(participant == 71) %>%
+  get_dupes(graph_image)
+
+mean(df$Age)
+sd(df$Age)
+
+unique(participant)
